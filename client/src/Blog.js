db7aba68599a937beb399afeac37ef39e404ce4f
lauricha <lauricha@cisco.com>
2023-11-13 14:28:23 -0500

    deployment

---
+++ client/src/Blog.js
@@ -0,0 +1,10 @@
+import './Blog.css'
+
+function Blog () {
+
+    return (
+        <div className="Blog"></div>
+    )
+}
+
+export default Blog
---
+++ client/src/Resume.js
@@ -0,0 +1,10 @@
+import LLRResume from './LLRResume.png'
+import './Resume.css'
+
+function Resume () {
+    return(
+        <img className="resume"src={LLRResume} alt="My Resume"></img>
+    )
+}
+
+export default Resume
